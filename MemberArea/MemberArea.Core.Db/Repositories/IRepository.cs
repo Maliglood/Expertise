bd14b067af95e5385aff99c6ddb102aed67147cb
Andrew Zakharov <ZaharovA@tomskasu.ru>
2017-12-01 10:36:52 +0700

    Создание пустого проекта доступа к БД для личного кабинета.

---
+++ MemberArea/MemberArea.Core.Db/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace MemberArea.Core.Db.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        void Create(TEntity item);
+        TEntity FindById(int id);
+        IEnumerable<TEntity> Get();
+        IEnumerable<TEntity> Get(Func<TEntity, bool> predicate);
+        void Remove(TEntity item);
+        void Update(TEntity item);
+    }
+}
